d555aaf8a4949a1aaa2e639cbb31218cc60e7653
Fajit <49131566+JaybirdDotSe@users.noreply.github.com>
2022-04-22 14:01:50 +0200

    Added token required and generate token JWT functionality

--- blueprints/open/__init__.py
+++ blueprints/open/__init__.py
@@ -1,16 +1,46 @@
 from flask import Blueprint
 from models import User
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import jwt
+import datetime
+from functools import wraps
 
 bp_open = Blueprint('bp_open', __name__)
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, '123secret', algorigtms=['HS256'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
 @bp_open.get('/user')
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
     users = User.query.all()
+
     output = []
     for user in users:
         user_data = {}
@@ -23,7 +53,8 @@ def get_all_users():
 
 
 @bp_open.get('/user/<public_id>')
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -39,7 +70,8 @@ def get_one_user(public_id):
 
 
 @bp_open.post('/user')
-def create_user():
+@token_required
+def create_user(current_user):
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
@@ -50,7 +82,8 @@ def create_user():
 
 
 @bp_open.put('/user/<public_id>')
-def promote_user(public_id):
+@token_required
+def promote_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -64,7 +97,8 @@ def promote_user(public_id):
 
 
 @bp_open.delete('/user/<public_id>')
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -75,3 +109,24 @@ def delete_user(public_id):
     db.session.commit()
 
     return jsonify({'message': 'The user has been deleted!'})
+
+
+@bp_open.get('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode(
+            {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+            '123secret')
+        return jsonify({'token': token})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
