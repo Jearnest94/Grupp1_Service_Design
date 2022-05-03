6b21c74960365a6e1ad32ed3805b7fb065956d4c
CharlotteAndreasson <charlotte.andreasson@student.kyh.se>
2022-05-03 14:08:25 +0200

    Two movie tests completed

--- src/test/test_movies.py
+++ src/test/test_movies.py
@@ -3,6 +3,7 @@ Unit tests for movie endpoint
 """
 
 import json
+import jsonpath
 from .fixture import client
 
 def test_get_movie_status_code(client):
@@ -20,9 +21,13 @@ def test_get_movies(client):
     :param client: App test client from fixture
     :return: None
     """
-
     response = client.get('/api/v1.0/movie')
     data = json.loads(response.text)
+    test = data['movie']
+    first_film = next((item for item in test if item['movie_id'] == 1), None)
+    assert first_film["Series_Title"] == "The Shawshank Redemption"
 
-    # GAAAAHHHHHH
+def test_post_movie(client):
+    pass
+
 
