d75e1d4339659f609805c0ab5e45d9224abb49b2
CharlotteAndreasson <charlotte.andreasson@student.kyh.se>
2022-05-10 11:54:09 +0200

    Swagger documentation completed for all endpoints

--- blueprints/user/__init__.py
+++ blueprints/user/__init__.py
@@ -17,7 +17,7 @@ bp_user = Blueprint('bp_user', __name__)
 @token_required
 def get_all_users(current_user):
     if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function! Check your privilegies.'}), 403
+        return jsonify({'message': 'Cannot perform that function! Check your privileges.'}), 403
 
     users = User.query.all()
 
@@ -36,7 +36,7 @@ def get_all_users(current_user):
 @token_required
 def get_one_user(current_user, id):
     if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function! Check your privilegies.'}), 403
+        return jsonify({'message': 'Cannot perform that function! Check your privileges.'}), 403
 
     user = User.query.filter_by(id=id).first()
 
@@ -56,7 +56,7 @@ def get_one_user(current_user, id):
 @token_required
 def create_user(current_user):
     if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function! Check your privilegies.'}), 403
+        return jsonify({'message': 'Cannot perform that function! Check your privileges.'}), 403
 
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
@@ -65,14 +65,14 @@ def create_user(current_user):
     from app import db
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'message': f'User with username {username} created!'}), 200
+    return jsonify({'message': f'User with username {username} created!'}), 201
 
 
 @bp_user.put('/user/<id>')
 @token_required
 def promote_user(current_user, id):
     if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function! Check your privilegies.'}), 403
+        return jsonify({'message': 'Cannot perform that function! Check your privileges.'}), 403
 
     user = User.query.filter_by(id=id).first()
 
